9d42ea9a557e0c7c8b1e04ba008cfabee206f2e7
Hans Krupakar <hanskrupakar1994@gmail.com>
2017-04-13 01:43:11 +0530

    Corrected Y values

--- data.py
+++ data.py
@@ -44,13 +44,9 @@ def preprocess(name):
 
 def create_batches(data15, batch_size, pmin, pmax):
 	
-	X=[]
-	Y=[]
-	x_batch = []
-	y_batch = []
-	prec=[]
+	X, Y, x_batch, y_batch, prec = [], [], [], [], []
 	
-	total = np.shape(data15)[0] - 23
+	total = np.shape(data15)[0] - 47
 	total = total - total%batch_size
 	
 	for i in xrange(total):	
@@ -64,7 +60,7 @@ def create_batches(data15, batch_size, pmin, pmax):
 		
 		#take 24 15min readings for 6 hour cumulative precipitation results
 		x_batch.append(data15[i:i+24, :15])
-		y_batch.append(np.reshape(np.sum(data15[i:i+24, 15]), (-1, 1)))
+		y_batch.append(np.sum(data15[i+24:i+48, 15]))
 	
 	return np.array(X), np.array(Y)
 		
@@ -76,29 +72,25 @@ def main():
 	
 	data, minimum, maximum = preprocess('Agrimet 15min.csv')
 	
-	np.random.shuffle(data)
-	
 	x_batches, y_batches = create_batches(data, batch_size, minimum[15], maximum[15])
 	
-	ntrain, nval, ntest = 0.9, 0.05, 0.05
+	ntrain, ntest = 0.9, 0.1
 	
 	tno = int(math.ceil(np.shape(x_batches)[0]*ntrain))
-	vno = int(math.ceil(np.shape(x_batches)[0]*nval))
-	testno = np.shape(x_batches)[0]-tno-vno
+	testno = np.shape(x_batches)[0]-tno
 	
-	print 'TRAIN BATCHES (%d):'%batch_size ,tno, '\nVAL BATCHES (%d):'%batch_size, vno, '\nTEST BATCHES (%d):'%batch_size, testno
+	print 'TRAIN BATCHES (%d):'%batch_size ,tno, '\nTEST BATCHES (%d):'%batch_size, testno
 	
-	trainx, valx, testx = x_batches[:tno], x_batches[tno:tno+vno], x_batches[tno+vno:]
-	trainy, valy, testy = y_batches[:tno], y_batches[tno:tno+vno], y_batches[tno+vno:]
+	trainx, testx = x_batches[:tno], x_batches[tno:]
+	trainy, testy = y_batches[:tno], y_batches[tno:]
 	
 	with h5py.File('minmax_full_dataset.h5', 'w') as f:
 		f.create_dataset('train/x', data=trainx)
 		f.create_dataset('train/y', data=trainy)
-		f.create_dataset('val/x', data=valx)
-		f.create_dataset('val/y', data=valy)
 		f.create_dataset('test/x', data=testx)
 		f.create_dataset('test/y', data=testy)
 		f.create_dataset('min', data=minimum)
 		f.create_dataset('max', data=maximum)
-	
-main()
+
+if __name__ == "__main__":	
+	main()
